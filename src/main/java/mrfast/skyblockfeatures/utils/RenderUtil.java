70168ddde497ea38046b508a2df68698865e669d
MrFast <jedijbc@gmail.com>
2022-07-15 14:07:12 -0400

    cleanup

--- src/main/java/mrfast/skyblockfeatures/utils/RenderUtil.java
+++ src/main/java/mrfast/skyblockfeatures/utils/RenderUtil.java
@@ -99,59 +99,6 @@ public class RenderUtil {
         tessellator.draw();
      }
 
-
-    public static void drawLines(List<BlockPos> poses, AColor colour, float thickness, float partialTicks, boolean depth) {
-        Entity render = Minecraft.getMinecraft().getRenderViewEntity();
-        WorldRenderer worldRenderer = Tessellator.getInstance().getWorldRenderer();
-
-        double realX = render.lastTickPosX + (render.posX - render.lastTickPosX) * partialTicks;
-        double realY = render.lastTickPosY + (render.posY - render.lastTickPosY) * partialTicks;
-        double realZ = render.lastTickPosZ + (render.posZ - render.lastTickPosZ) * partialTicks;
-
-        GlStateManager.pushMatrix();
-        GlStateManager.translate(-realX, -realY, -realZ);
-        GlStateManager.disableTexture2D();
-        GlStateManager.disableLighting();
-        GlStateManager.enableBlend();
-        GlStateManager.disableAlpha();
-        GL11.glLineWidth(thickness);
-        if (!depth) {
-            GlStateManager.disableDepth();
-            GlStateManager.depthMask(false);
-        }
-        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
-
-        GlStateManager.color(1,1,1,1);
-        worldRenderer.begin(GL11.GL_LINE_STRIP, DefaultVertexFormats.POSITION_COLOR);
-        int num = 0;
-        for (BlockPos pos: poses) {
-            int i = getColorAt(num++ * 10,0, colour);
-            worldRenderer.pos(pos.getX() +0.5, pos.getY() +0.5, pos.getZ() +0.5).color(
-                    ((i >> 16) &0xFF)/255.0f,
-                    ((i >> 8) &0xFF)/255.0f,
-                    (i &0xFF)/255.0f,
-                    ((i >> 24) &0xFF)/255.0f
-            ).endVertex();
-        }
-        Tessellator.getInstance().draw();
-
-        GlStateManager.translate(realX, realY, realZ);
-        GlStateManager.disableBlend();
-        GlStateManager.enableAlpha();
-        GlStateManager.enableTexture2D();
-        if (!depth) {
-            GlStateManager.enableDepth();
-            GlStateManager.depthMask(true);
-        }
-        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-        GlStateManager.popMatrix();
-        GL11.glLineWidth(1);
-    }
-
-    public static int getColorAt(double x, double y, AColor color) {
-        return color.getRGB();
-    }
-
     /**
      * Taken from NotEnoughUpdates under Creative Commons Attribution-NonCommercial 3.0
      * https://github.com/Moulberry/NotEnoughUpdates/blob/master/LICENSE
